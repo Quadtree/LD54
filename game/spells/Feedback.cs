a988464c4d6eac7610d5ac556b7d5ae86123c0dc
Quadtree <quadtree@gmail.com>
2023-09-30 10:47:44 -0700

    U

--- game/spells/Feedback.cs
+++ game/spells/Feedback.cs
@@ -5,7 +5,7 @@ public class Feedback : Spell
 {
     private static IReadOnlyList<IntVec2> _Footprint = new IntVec2[]{
         new IntVec2(0,0),
-        new IntVec2(1,0),
+        //new IntVec2(1,0),
     };
 
     public override IReadOnlyList<IntVec2> Footprint => _Footprint;
@@ -14,7 +14,7 @@ public class Feedback : Spell
     {
         if (!base.FinishCasting(caster, target, pos)) return false;
 
-        target.TakeDamage(10);
+        target.TakeDamage(8);
 
         return true;
     }
@@ -23,11 +23,11 @@ public class Feedback : Spell
     {
         if (!Footprint.All(it => grid.IsCellOpen(point + it))) return false;
 
-        for (var x = point.x - 1; x <= point.x + 2; ++x)
+        for (var x = point.x - 1; x <= point.x + 1; ++x)
         {
-            for (var y = point.y - 1; x <= point.y + 1; ++y)
+            for (var y = point.y - 1; y <= point.y + 1; ++y)
             {
-                if (!enemyGrid.CellsUsed[x, y]) return false;
+                if (!enemyGrid.IsCellInBounds(new IntVec2(x, y)) || !enemyGrid.CellsUsed[x, y]) return false;
             }
         }
 
