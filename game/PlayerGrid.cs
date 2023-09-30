d62b1496663db9b4f92eeb12be5d9b1bd157c71c
Quadtree <quadtree@gmail.com>
2023-09-29 17:58:47 -0700

    U

--- game/PlayerGrid.cs
+++ game/PlayerGrid.cs
@@ -2,4 +2,32 @@ public class PlayerGrid
 {
     public int Width => 7;
     public int Height => 7;
+
+    public bool[,] CellsAvailable;
+    public bool[,] CellsUsed;
+
+    public PlayerGrid()
+    {
+        CellsAvailable = new bool[Width, Height];
+
+        // temporary!
+        for (var y = 0; y < Height; ++y)
+        {
+            for (var x = 0; x < Width; ++x)
+            {
+                CellsAvailable[x, y] = true;
+            }
+        }
+
+        CellsUsed = new bool[Width, Height];
+    }
+
+    public bool IsCellOpen(IntVec2 pos)
+    {
+        if (pos.x < 0 || pos.y < 0 || pos.x >= Width || pos.y >= Height) return false;
+
+        if (CellsAvailable[pos.x, pos.y] && !CellsUsed[pos.x, pos.y]) return true;
+
+        return false;
+    }
 }
--- game/spells/Spell.cs
+++ game/spells/Spell.cs
@@ -1,9 +1,15 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public class Spell
 {
     public virtual string Name => this.GetType().ToString();
 
     public virtual IReadOnlyList<IntVec2> Footprint => Array.Empty<IntVec2>();
+
+    public virtual bool IsValidAtPoint(IntVec2 point, PlayerGrid grid)
+    {
+        return Footprint.All(it => grid.IsCellOpen(point + it));
+    }
 }
