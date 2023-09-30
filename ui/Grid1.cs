304e4740e5ecf724fb06078264524fb746190bd1
Quadtree <quadtree@gmail.com>
2023-09-29 19:33:14 -0700

    U

--- ui/Grid1.cs
+++ ui/Grid1.cs
@@ -8,9 +8,18 @@ public class Grid1 : GridContainer
 
     public List<Action<IntVec2>> CellClickedListeners = new List<Action<IntVec2>>();
 
+    public enum HoverType
+    {
+        SpellBase,
+    }
+
+    public Func<IEnumerable<Tuple<IntVec2, HoverType>>> HoveredCellsSource = () => Array.Empty<Tuple<IntVec2, HoverType>>();
+
     TextureRect Template;
 
     bool Initialized = false;
+
+    TextureRect[,] Cells;
 
     public override void _Ready()
     {
@@ -26,6 +35,8 @@ public class Grid1 : GridContainer
 
             Columns = grid.Width;
 
+            Cells = new TextureRect[grid.Width, grid.Height];
+
             for (var y = 0; y < grid.Height; ++y)
             {
                 for (var x = 0; x < grid.Width; ++x)
@@ -33,6 +44,7 @@ public class Grid1 : GridContainer
                     var nu = Template.Duplicate();
                     nu.Connect("gui_input", this, nameof(GridCellGUIInput), new Godot.Collections.Array(new object[] { x, y }));
                     AddChild(nu);
+                    Cells[x, y] = (TextureRect)nu;
                 }
             }
 
@@ -40,6 +52,8 @@ public class Grid1 : GridContainer
 
             GD.Print($"Grid initialized with {grid.Width}x{grid.Height}");
         }
+
+
     }
 
     public void GridCellGUIInput(InputEvent evt, int x, int y)
