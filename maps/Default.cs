5bbc47ad604e27bf8040fec986c44e3dd5680fa1
Quadtree <quadtree@gmail.com>
2023-09-29 17:34:38 -0700

    U

--- maps/Default.cs
+++ maps/Default.cs
@@ -3,14 +3,12 @@ using System;
 
 public class Default : Node2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+    MatchState MS = new MatchState();
 
-    // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        this.FindChildByName<Grid1>("Grid1").Src = () => MS.PlayerGrid;
+        this.FindChildByName<Grid1>("Grid2").Src = () => MS.OpponentGrid;
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
--- ui/Grid1.cs
+++ ui/Grid1.cs
@@ -3,28 +3,33 @@ using System;
 
 public class Grid1 : GridContainer
 {
-    Func<PlayerGrid> Src = () => null;
+    public Func<PlayerGrid> Src = () => null;
+
+    TextureRect Template;
+
+    bool Initialized = false;
 
     public override void _Ready()
     {
-        var template = this.FindChildByName<TextureRect>("Template");
-        template.GetParent().RemoveChild(template);
-
-        var grid = Src();
-
-        Columns = grid.Width;
-
-        for (var y = 0; y < grid.Height; ++y)
-        {
-            for (var x = 0; x < grid.Width; ++x)
-            {
-                AddChild(template.Duplicate());
-            }
-        }
+        Template = this.FindChildByName<TextureRect>("Template");
+        Template.GetParent().RemoveChild(Template);
     }
 
     public override void _Process(float delta)
     {
+        if (!Initialized && Src() != null)
+        {
+            var grid = Src();
 
+            Columns = grid.Width;
+
+            for (var y = 0; y < grid.Height; ++y)
+            {
+                for (var x = 0; x < grid.Width; ++x)
+                {
+                    AddChild(Template.Duplicate());
+                }
+            }
+        }
     }
 }
