64d5290cda6a3cb9bd743c9673fbbdd72d728dd2
Quadtree <quadtree@gmail.com>
2023-09-29 23:10:46 -0700

    U

--- maps/Default.cs
+++ maps/Default.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 public class Default : Node2D
@@ -14,6 +15,12 @@ public class Default : Node2D
 
     CombatantStatus[] CS;
 
+    IEnumerator<bool> Operation;
+
+    BasicAI AI = new BasicAI();
+
+    public bool IsCurrentlyPlayersTurn => (MS.CurrentTurn == 0 && MS.CurrentPhase == MatchState.Phase.Main) || (MS.CurrentTurn == 1 && MS.CurrentPhase == MatchState.Phase.Reaction);
+
     public override void _Ready()
     {
         var c1 = this.FindChildByName<CombatantStatus>("CombatantStatus");
@@ -23,15 +30,28 @@ public class Default : Node2D
 
         CS = new CombatantStatus[] { c1, c2 };
 
-        c1.CellClickedListeners.Add(v2 => MS.TryCastSpell(SelectedSpell, 0, 1, v2));
+        c1.CellClickedListeners.Add(v2 => { if (IsCurrentlyPlayersTurn) MS.TryCastSpell(SelectedSpell, 0, 1, v2); });
 
         // @TODO: Remove me
-        c2.CellClickedListeners.Add(v2 => MS.TryCastSpell(SelectedSpell, 1, 0, v2));
+        c2.CellClickedListeners.Add(v2 => { if (IsCurrentlyPlayersTurn) MS.TryCastSpell(SelectedSpell, 1, 0, v2); });
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(float delta)
     {
+        if (Operation != null)
+        {
+            if (Operation.MoveNext()) Operation = null;
+        }
+
+        if (MS.CurrentTurn == 1 && MS.CurrentPhase == MatchState.Phase.Main)
+        {
+            Operation = AI.RunTurn(MS.CurrentTurn, MS).GetEnumerator();
+        }
+
+        // stop the player from doing anything if it's not their turn
+        if (!IsCurrentlyPlayersTurn) return;
+
         for (var i = 0; i < 1; ++i)
         {
             if (Input.IsActionJustPressed($"select_spell_{i}"))
@@ -55,6 +75,8 @@ public class Default : Node2D
             //GD.Print("CUR HOVER! " + string.Join(" ", SelectedSpell.Footprint.Select(it => Tuple.Create(it + curHoverNotNull, Grid1.HoverType.SpellBase))));
             CS[MS.CurrentTurn].Grid1.HoveredCellsSource = () => SelectedSpell.Footprint.Select(it => Tuple.Create(it + curHoverNotNull, Grid1.HoverType.SpellBase)).ToArray();
         }
+
+
     }
 
 
