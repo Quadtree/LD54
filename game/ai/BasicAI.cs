eea4d58426855144e6d5b7caf2f5698e7a0b3bad
Quadtree <quadtree@gmail.com>
2023-09-29 23:05:10 -0700

    U

--- game/ai/BasicAI.cs
+++ game/ai/BasicAI.cs
@@ -1,9 +1,17 @@
+using System.Collections.Generic;
+
 public class BasicAI
 {
-    public void RunTurn(int myId, MatchState ms)
+    public IEnumerable<bool> RunTurn(int myId, MatchState ms)
     {
         var opoId = 1 - myId;
 
-        
+        for (var i = 0; i < 1000; ++i)
+        {
+            var trgPos = new IntVec2(
+                Util.RandInt(0, ms.Combatants[myId].Grid.Width),
+                Util.RandInt(0, ms.Combatants[myId].Grid.Height)
+            );
+        }
     }
 }
