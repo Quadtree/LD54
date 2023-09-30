16fb84cd8d2183bd3339caaff5b241c07d30991d
Quadtree <quadtree@gmail.com>
2023-09-29 18:02:28 -0700

    U

--- maps/Default.cs
+++ maps/Default.cs
@@ -5,6 +5,12 @@ public class Default : Node2D
 {
     MatchState MS = new MatchState();
 
+    Spell SelectedSpell;
+
+    Spell[] PossibleSpells = new Spell[]{
+        new BurningBolt()
+    };
+
     public override void _Ready()
     {
         this.FindChildByName<CombatantStatus>("CombatantStatus").Src = () => MS.Combatants[0];
@@ -14,6 +20,10 @@ public class Default : Node2D
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(float delta)
     {
-
+        for (var i = 0; i < 1; ++i) if (Input.IsActionJustPressed($"select_spell_{i}"))
+            {
+                SelectedSpell = PossibleSpells[i];
+                GD.Print($"Spell {SelectedSpell.Name} selected");
+            }
     }
 }
