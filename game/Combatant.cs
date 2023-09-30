857044a8a580a2389b69c88436ea37c0fd79fd5b
Quadtree <quadtree@gmail.com>
2023-09-30 10:16:19 -0700

    U

--- game/Combatant.cs
+++ game/Combatant.cs
@@ -1,7 +1,33 @@
+using Godot;
+
 public class Combatant
 {
     public int HP = 15;
     public int SP = 1;
+    public int Shield = 0;
+
+    public string Name;
 
     public PlayerGrid Grid = new PlayerGrid();
+
+    public void TakeDamage(int amt)
+    {
+        if (amt > Shield && Shield > 0)
+        {
+            amt -= Shield;
+            GD.Print($"{Name} takes {amt} damage, {Shield} absorbed by shield");
+            Shield = 0;
+            HP -= amt;
+        }
+        else if (Shield > 0)
+        {
+            GD.Print($"{Name}'s shield absorbs {amt} damage");
+            Shield -= amt;
+        }
+        else
+        {
+            GD.Print($"{Name} takes {amt} damage");
+            HP -= amt;
+        }
+    }
 }
