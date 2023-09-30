b2f720d1edb1f903294e1d254c945a0a24cc1392
Quadtree <quadtree@gmail.com>
2023-09-30 11:31:40 -0700

    U

--- maps/Default.cs
+++ maps/Default.cs
@@ -177,5 +177,7 @@ public class Default : Node2D
         }
 
         MS.KnownSpells = level.KnownSpells;
+        MS.Combatants[0].HP = level.PlayerHP;
+        MS.Combatants[1].HP = level.EnemyHP;
     }
 }
