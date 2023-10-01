700fb59a9a2783a41791c1e610756c633950d5b7
Quadtree <quadtree@gmail.com>
2023-09-30 20:48:44 -0700

    U

---
+++ ui/SpellCard.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public class SpellCard : VBoxContainer
+{
+    public Spell Spell;
+
+    public override void _Ready()
+    {
+
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        if (Spell is Spell spell)
+        {
+            this.FindChildByName<Label>("Name").Text = spell.Name;
+        }
+    }
+}
