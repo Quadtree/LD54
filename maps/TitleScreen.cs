663dd2fdb6ee05c8d190a66ef2ce3c0b8d73d0ee
Quadtree <quadtree@gmail.com>
2023-09-30 19:41:09 -0700

    U

--- maps/TitleScreen.cs
+++ maps/TitleScreen.cs
@@ -10,12 +10,17 @@ public class TitleScreen : Control
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        this.FindChildByName<Button>("StartGameButton").Connect("pressed", this, nameof(OnStartGameButton));
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    void OnStartGameButton()
+    {
+        GetTree().ChangeScene("res://maps/Default.tscn");
+    }
+
+    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    //  public override void _Process(float delta)
+    //  {
+    //
+    //  }
 }
