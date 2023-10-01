34a7586d59db1edaf358e7eb41ebc1e0e6507b66
Quadtree <quadtree@gmail.com>
2023-09-30 19:48:13 -0700

    U

--- maps/Default.cs
+++ maps/Default.cs
@@ -308,6 +308,9 @@ public class Default : Control
         MS.Combatants[1].HP = level.EnemyHP;
         MS.AIMinSPToCast = level.AIMinSPToCast;
         MS.EnemyAvailableSpells = level.EnemySpells.Select(it => PossibleSpells[(int)it]).ToArray();
+
+        CS[0].FindChildByName<Sprite>("Sprite2").Modulate = Colors.SandyBrown;
+        CS[1].FindChildByName<Sprite>("Sprite2").Modulate = level.EnemyMageRobeColor;
     }
 
     public void AddSpellInFlight(int fromId, int toId, string texture)
